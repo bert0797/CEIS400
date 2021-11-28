f329ab338e4cb6c8fe503a62d39b471e5224efc0
bert0797 <95158176+bert0797@users.noreply.github.com>
2021-11-27 21:20:36 -0800

    Create Manager.h

---
+++ Manager.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Manager
+{
+private:
+	string strFName;
+	string strLName;
+	string strDepartment;
+	int	intNumEmployees;
+
+public:
+	//Constructor_Destructor
+	Manager(void);
+	~Manager(void);
+};
+

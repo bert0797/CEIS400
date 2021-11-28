7fba549c649e3b0194b3faf23d75a462dfeb6ad7
bert0797 <95158176+bert0797@users.noreply.github.com>
2021-11-27 21:17:43 -0800

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,13 +1,48 @@
-/*
-Matthew Smith
-November 25, 2021
-CEIS 400 Week 5
-*/
+/**********************************************************************************
+***																				                                      ***
+***	Robert Bertenshaw														                               	***
+***	Kandyce Davis															                                	***
+***	Danielle Henry															                              	***
+***	Matthew Smith															                                	***
+***																				                                      ***
+***	Group C	Project															                               	***
+***	CEIS 400-0-0															                                	***
+***	October 2021 to December 2021											                        	***
+***																				                                      ***
+**********************************************************************************/
+
+/**********************************************************************************
+***																			                                      	***
+***	Scope:																		                                  ***
+***	The purpose of this program is to automate the check-out and check-in	    	***
+***	process of equipment and supplies from the equipment depot.	This application***
+***	will track and record transactions between the depot and the requesting		  ***
+***	technician.  As a result, the information will be recorded to allow for	  	***
+***	tracking of equipment.  This will increase visiblity of equipment loss by	  ***
+***	requestor.  Reports can be generated to identify trends by department and	  ***
+***	technician.																	                                ***
+***																				                                      ***
+**********************************************************************************/
+
 
 #include <iostream>
-#include <string>
+#include <string.h>
+#include "Equipment.h"
+#include "Manager.h"
+#include "Technician.h"
 
 using namespace std;
 
-///Entry point for the application
-int main()
+int main(void){
+
+	//Variable Declaration and Initialization
+
+
+	//User Input
+	
+	//Function Request : Check-out, Check-in, Technician Report, Manager Report
+
+	//Open and update fileA
+	return 0;
+}
+

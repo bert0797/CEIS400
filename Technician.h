3d8959785eb6ded342744b099ea5982a9789992f
bert0797 <95158176+bert0797@users.noreply.github.com>
2021-11-27 21:19:04 -0800

    Create Technician.h

---
+++ Technician.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+#include "Manager.h"
+
+using namespace std;
+
+
+class Technician :
+	public Manager
+{
+private:
+	string strFName;
+	string strLName;
+	string strDepartment;
+	double dblEmployeeNumber;
+	Manager manName;
+
+public:
+	//Constructor_Destructor
+	Technician(void);
+	~Technician(void);
+	Technician(string,string, double);
+
+	//Mutator
+	void setName (string, string);
+	string getName (void);
+
+
+};
+
